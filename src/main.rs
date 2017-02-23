487e7f9dccbb8e2e69c99c35c89585f6ada91365
Pascal Seitz <pascal.seitz@gmail.com>
2017-02-24 00:38:15 +0100

    all the regexes

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,12 @@ extern crate rand;
 extern crate futures;
 extern crate futures_cpupool;
 extern crate tokio_timer;
+extern crate regex;
+
+#[macro_use] extern crate lazy_static;
+
 // extern crate rustc_serialize;
+mod util;
 mod search;
 
 fn main() {
--- src/util.rs
+++ src/util.rs
@@ -1,36 +1,92 @@
 
-fn normalizeText(text:&str) -> String{
-    let newStr = text.to_owned();
-    {
-        let re = Regex::new("*\([fmn\d)]*").unwrap(); // remove (f)(n)(m)(1)...(9)
-        newStr = re.replace(newStr, "");
-    }
-    {
-        let re = Regex::new("[\(\)]").unwrap();  // remove braces
-        newStr = re.replace(newStr, "");
-    }
-    {
-        let re = Regex::new("[{}'\"“]").unwrap(); // remove ' " {}
-        newStr = re.replace(newStr, "");
-    }
-    {
-        let re = Regex::new("\s\s+").unwrap(); // replace tabs, newlines, double spaces with single spaces
-        newStr = re.replace(newStr, " ");
-    }
-    {
-        let re = Regex::new("[,.…]").unwrap();  // remove , .
-        newStr = re.replace(newStr, "");
-    }
-    {
-        let re = Regex::new("[;・’-]").unwrap(); // remove ;・’-
-        newStr = re.replace(newStr, "");
+use regex::Regex;
+
+use std::borrow::Cow;
+pub  fn normalizeText(text:&str) {
+    let mut newStr = text.to_owned();
+
+    lazy_static! {
+        static ref REGEXES:Vec<(Regex, & 'static str)> = vec![
+            (Regex::new(r"*\([fmn\d])*").unwrap(), " "),
+            (Regex::new(r"[\(\)]").unwrap(), " "),  // remove braces
+            (Regex::new(r#"[{}'"“]"#).unwrap(), ""), // remove ' " {}
+            (Regex::new(r"\s\s+").unwrap(), " "), // replace tabs, newlines, double spaces with single spaces
+            (Regex::new(r"[,.…]").unwrap(), ""),  // remove , .
+            (Regex::new(r"[;・’-]").unwrap(), "") // remove ;・’-
+        ];
+
+        // static ref EEEE:Vec<(& 'static str)> = vec![
+        //     (" "),
+        //     (" "),  // remove braces
+        //     ( ""), // remove ' " {}
+        //     (""), // replace tabs, newlines, double spaces with single spaces
+        //     (""),  // remove , .
+        //     ("") // remove ;・’-
+        // ];
     }
 
-    text = text.toLowerCase()
-    return text.trim()
+    // for a in &*EEEE {
+    //     newStr = a;
+    //     // let seq = seq_arc.clone();
+    //     // let restr = variant.to_string();
+    //     // let future = thread::spawn(move || variant.find_iter(&seq).count());
+    //     // counts.push((restr, future));
+    // }
+
+    for ref tupl in &*REGEXES {
+        newStr = (tupl.0).replace(&newStr, tupl.1).into_owned();
+        // let seq = seq_arc.clone();
+        // let restr = variant.to_string();
+        // let future = thread::spawn(move || variant.find_iter(&seq).count());
+        // counts.push((restr, future));
+    }
+
+    // let yoo :Vec<(Regex, & 'static str)> = vec![
+    //         (Regex::new(r"*\([fmn\d])*").unwrap(), " "),
+    //         (Regex::new(r"[\(\)]").unwrap(), " "),  // remove braces
+    //         (Regex::new(r#"[{}'"“]"#).unwrap(), ""), // remove ' " {}
+    //         (Regex::new(r"\s\s+").unwrap(), " "), // replace tabs, newlines, double spaces with single spaces
+    //         (Regex::new(r"[,.…]").unwrap(), ""),  // remove , .
+    //         (Regex::new(r"[;・’-]").unwrap(), "") // remove ;・’-
+    //     ];
+
+    // for (regex, b) in yoo {
+    //     newStr = regex.replace(&newStr, b).into_owned();
+    // }
+    // {
+    //     lazy_static! {
+    //         static ref re: Regex = Regex::new("*\([fmn\d)]*").unwrap();
+    //     }
+    //     // let re = Regex::new("*\([fmn\d)]*").unwrap(); // remove (f)(n)(m)(1)...(9)
+    //     newStr = re.replace(&newStr, "");
+    // }
+    // {
+    //     let re = Regex::new("[\(\)]").unwrap();  // remove braces
+    //     newStr = re.replace(&newStr, "");
+    // }
+    // {
+    //     let re = Regex::new("[{}'\"“]").unwrap(); // remove ' " {}
+    //     newStr = re.replace(&newStr, "");
+    // }
+    // {
+    //     let re = Regex::new("\s\s+").unwrap(); // replace tabs, newlines, double spaces with single spaces
+    //     newStr = re.replace(&newStr, " ");
+    // }
+    // {
+    //     let re = Regex::new("[,.…]").unwrap();  // remove , .
+    //     newStr = re.replace(&newStr, "");
+    // }
+    // {
+    //     let re = Regex::new("[;・’-]").unwrap(); // remove ;・’-
+    //     newStr = re.replace(&newStr, "");
+    // }
+
+    // text = text.toLowerCase()
+    // return text.trim()
+
+
 
 }
-
 // assert_eq!(re.replace("1078910", ""), " ");
 
 //     text = text.replace(/ *\([^)]*\) */g, ' ') // remove everything in braces
