b67595e69347406a052135e6c77a2132dbdca854
Pascal Seitz <pascal.seitz@gmail.com>
2018-09-21 17:26:17 +0200

    testing serde compact int

---
+++ compact_int/src/lib.rs
@@ -0,0 +1,29 @@
+
+extern crate serde;
+
+// The serde_derive crate provides the macros for #[derive(Serialize)] and
+// #[derive(Deserialize)]. You won't need these for implementing a data format
+// but your unit tests will probably use them - hence #[cfg(test)].
+// #[cfg(test)]
+// #[macro_use]
+// extern crate serde_derive;
+
+// mod de;
+mod error;
+mod ser;
+
+// pub use de::{from_str, Deserializer};
+pub use self::error::{Error, Result};
+// pub use ser::{to_string, Serializer};
+
+
+
+// mod ser;
+
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, 4);
+//     }
+// }
