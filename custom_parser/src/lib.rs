fb91c28e45a1573c4f03ccb74e06c4ac7315043f
Pascal Seitz <pascal.seitz@sap.com>
2020-04-30 18:06:19 +0200

    add customizable query parsing

--- custom_parser/src/lib.rs
+++ custom_parser/src/lib.rs
@@ -3,5 +3,17 @@
 mod lexer;
 pub mod parser;
 pub mod ast;
+pub mod error;
 
 pub use parser::parse;
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Options {
+    /// This setting will disable parsing of the attribute specfier "attr:"
+    pub no_attributes: bool,
+    /// This setting will disable parsing of the parentheses
+    pub no_parentheses: bool,
+    /// This setting will disable defining a levensthtein distance after a searchterm, e.g. "searchterm~2"
+    pub no_levensthein: bool,
+    // pub no_quotes: bool,
+}
